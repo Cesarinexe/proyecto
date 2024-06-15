2f21b5debbf15056e2f93a3a7e1ba003b6099215
Cesarinexe <cesarcrespo100@gmail.com>
2024-06-14 21:35:19 -0400

    incorpororacion de calculadora 1

--- calculadora.py
+++ calculadora.py
@@ -1,27 +1,29 @@
 num1 = int(input("numero 1: ")) 
 num2 = int(input("numero 2: ")) 
 
+valor = 0
 while True:
     print("""seleccione opcion
             1- Sumar 
             2- Restar
             3- Multiplicar
-            4- Dividir 
+            4- dividir 
         """)
 
     valor = int(input("Elige una opcion: ") )     
 
     if valor == 1:
-        print("La suma es", num1 + num2)
-        break
-    elif valor == 2:
-        print("La resta es", num1 - num2)
-        break
-    elif valor == 3:
-        print("La multiplicacion es", num1 * num2)
-        break
-    elif valor == 4:
-        print("La division es", num1 / num2)
-        break
+        print("la suma es",num1+num2)
+        break;
+    if valor == 2:
+        print("la resta es",num1-num2)
+        break;
+    if valor == 3:
+        print("la multiplicacion es",num1*num2)
+        break;
+    if valor == 4:
+        print("la division es",num1/num2)
+        break;
     else:
-        print("Opcion incorrecta, por favor intente de nuevo.")
+        print("Opcion incorrecta")
+        break;
